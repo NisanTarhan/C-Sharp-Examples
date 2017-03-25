cc14b4125decf4f9df0004ab6873a87f940ccc22
Nisan Tarhan <nisan_tarhan@hotmail.com>
2017-03-25 17:44:03 +0200

    FaktoriyelHesaplama dosyası eklendi

---
+++ FaktoriyelHesaplama/deneme12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deneme12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(long sayac=1;sayac<=10;++sayac)
+            {
+                Console.WriteLine($"{sayac}! = {Faktoriyel(sayac)}");
+            }
+            Console.ReadKey();
+        }
+
+        static long Faktoriyel (long number)
+        {
+            if (number <= 1)
+                return 1;
+            else
+                return number * Faktoriyel(number - 1);
+        }
+    }
+}
