2ac502ccc6e3d96a014f69cb17ae2cb16c0fecd1
Nisan Tarhan <nisan_tarhan@hotmail.com>
2017-08-17 18:34:19 +0300

    PalindromikSayilar
    Palindromik Sayilar : 121, 212, 313, 131, ...

---
+++ PalindromikSayilar/PalindromikSayilar/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PalindromikSayilar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.PalindromikSayilar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(286, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(180, 324);
+            this.listBox1.TabIndex = 0;
+            // 
+            // PalindromikSayilar
+            // 
+            this.PalindromikSayilar.Location = new System.Drawing.Point(25, 81);
+            this.PalindromikSayilar.Name = "PalindromikSayilar";
+            this.PalindromikSayilar.Size = new System.Drawing.Size(141, 62);
+            this.PalindromikSayilar.TabIndex = 1;
+            this.PalindromikSayilar.Text = "Palindromik";
+            this.PalindromikSayilar.UseVisualStyleBackColor = true;
+            this.PalindromikSayilar.Click += new System.EventHandler(this.PalindromikSayilar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 372);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PalindromikSayilar);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button PalindromikSayilar;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PalindromikSayilar/PalindromikSayilar/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PalindromikSayilar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PalindromikSayilar_Click(object sender, EventArgs e)
+        {
+            string metin = textBox1.Text;
+
+            string[] metin_parcalari = metin.Split(' ');
+
+            foreach (string parca in metin_parcalari)
+            {
+                char[] yeni = parca.ToCharArray();
+
+                Array.Reverse(yeni);
+
+                if (string.Compare(parca, new string(yeni)).ToString() == "0")
+                {
+                    listBox1.Items.Add(parca);
+                }
+            }
+        }
+               
+    }
+}
